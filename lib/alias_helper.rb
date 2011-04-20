35055c8d6a5f7d5a1fdd88cd8664d7e9fc57757a
Umofomia <claw@alum.mit.edu>
2011-04-19 18:37:59 -0700

    Fixed Rails 3 support.

--- lib/alias_helper.rb
+++ lib/alias_helper.rb
@@ -1,8 +1,15 @@
-module ActiveSupport::CoreExtensions::Module
+# alias_method_chain was moved to Module in Rails 3.
+module_to_patch = if defined?(ActiveSupport::CoreExtensions)
+  ActiveSupport::CoreExtensions::Module
+else
+  Module
+end
+
+module_to_patch.class_eval do
 
   # Given a target method and the feature name, returns the names of aliases
-  # that ActiveSupport::CoreExtensions::Module#alias_method_chain typically
-  # defines for the target method and feature.
+  # that ActiveSupport's alias_method_chain typically defines for the target
+  # method and feature.
   def method_chain_aliases(target, feature)
     # Strip out punctuation on predicates or bang methods since
     # e.g. target?_without_feature is not a valid method name.
@@ -14,15 +21,15 @@ module ActiveSupport::CoreExtensions::Module
   end
 
   # Given a target method and the feature name, returns the name of the alias
-  # that ActiveSupport::CoreExtensions::Module#alias_method_chain typically
-  # defines for the target method with the feature.
+  # that ActiveSupport's alias_method_chain typically defines for the target
+  # method with the feature.
   def method_alias_with_feature(target, feature)
     method_chain_aliases(target, feature)[0]
   end
 
   # Given a target method and the feature name, returns the name of the alias
-  # that ActiveSupport::CoreExtensions::Module#alias_method_chain typically
-  # defines for the target method without the feature.
+  # that ActiveSupport's alias_method_chain typically defines for the target
+  # method without the feature.
   def method_alias_without_feature(target, feature)
     method_chain_aliases(target, feature)[1]
   end
